a81e0d736c45be41063c8f4246200fcb5ff6911c
Krishna Manohar <gkmr907@gmail.com>
2025-10-30 10:00:00 +0530

    Add files via upload (week-12)

--- WEEK-11/src/models/student.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const studentSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true},
-    branch:{type:String, required:true}
-})
-
-export const student = mongoose.model("student",studentSchema);
